94bf3b0ca212cd11d09493ed87bbd66148c6f2f3
Radithpra <120902818+Maisner0513@users.noreply.github.com>
2023-06-15 14:19:10 +0700

    Add files via upload

---
+++ vault.lua
@@ -0,0 +1,8 @@
+-- Debug first before you run the script
+-- credit : maisner.gt
+
+x = 0
+y = 0
+
+SendPacket(2,"action|dialog_return\ndialog_name|storageboxxtreme\ntilex|"..x.."|\ntiley|"..y.."|\nitemid|1|\nbuttonClicked|cancel\nitemcount|1\n" )
+log("`bmaisner.gt`0(`4bypass `0: `3complete`0)")
